792bc03726940c5c36e916c707f1f7ce0097206d
ZarRomM <86100277+ZarRomM@users.noreply.github.com>
2025-06-05 23:17:49 -0600

    Arreglo del DF

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
 import streamlit as st
 import seaborn as sns
-from scipy import stats
-
-url_data = "https://github.com/ZarRomM/Proyect-7-Streamlit/blob/main/vehicles_us_limpio.csv"
-# car_data = df_cd
-df_cd = pd.read_csv(url_data) # leer los datos
-st.dataframe(df_cd)
 
 # Título principal
 st.title('Análisis de Vehículos Usados en EE.UU.')
+
+# Lectura de datos
+url_data = "https://github.com/ZarRomM/Proyect-7-Streamlit/blob/main/vehicles_us_limpio.csv"
+# car_data = df_cd
+df_cd = pd.read_csv(url_data) # leer los datos
+
+# Muestra del DF
+st.header('DataFrame US Cars')
+st.dataframe(df_cd)
 
 # Encabezado
 st.header('Distribución de Precios')
